db212c185df9d33157ef163438a8c574db399299
carlostrapet <trapet_llamas@hotmail.com>
2018-01-29 16:00:42 +0000

    added unit tests for balance and tests passing

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,12 @@
 require "oystercard"
 
 describe Oystercard do
+  let(:oystercard) { Oystercard.new }
+  context '#initialize' do 
+
+    it 'has a default balance of 0' do 
+
+      expect(oystercard.balance).to eq 0
+    end
+  end
 end
