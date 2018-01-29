60632c67fc31aa9523d74960ea52a4c4cba213c0
marcusfgardiner <marcusfgardiner@gmail.com>
2018-01-29 16:46:31 +0000

    top_up guard condition with max balance working

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,8 @@
 
 class Oystercard
 
+  MAX_BALANCE = 90
+
   attr_reader :balance
 
   def initialize
@@ -8,6 +10,11 @@ class Oystercard
   end
 
   def top_up(amount)
-    balance += amount
+    pre_top_up_checks(amount)
+    @balance += amount
+  end
+
+  def pre_top_up_checks(amount)
+    raise "Can't top up, max balance reached" if (@balance + amount) > MAX_BALANCE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
-  let(:oystercard) { Oystercard.new }
   let(:amount) { 10 }
 
-  context '#initialize' do 
-    it 'has a default balance of 0' do 
-      expect(oystercard.balance).to eq 0
+  context '#initialize' do
+    it 'has a default balance of 0' do
+      expect(subject.balance).to eq 0
     end
   end
-  context '#top_up' do 
-    it 'tops up card with input balance' do 
-      oystercard.top_up(amount)
-      expect(oystercard.balance).to eq amount  
+  context '#top_up' do
+    it 'tops up card with input balance' do
+      expect { subject.top_up(amount) }.to change { subject.balance }.by amount
+    end
+
+    it 'raises an error if top up exceeds max balance' do
+      expect { subject.top_up(described_class::MAX_BALANCE + amount) }.to raise_error "Can't top up, max balance reached"
     end
   end
 end
