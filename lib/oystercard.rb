4a47e79b295ec0eb160a3a29309b4206ebd71a7a
Daniel Keen <keendaniel01@gmail.com>
2018-01-30 12:33:13 +0000

    Completed challenge #08: created touch in and out methods to change the cards in journey status

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,10 +3,11 @@ class Oystercard
 
   MAX_BALANCE = 90
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -21,5 +22,18 @@ class Oystercard
 
   def deduct_fare(amount)
     @balance -= amount
+  end
+
+  def touch_in
+    # change_journey_status
+    @in_journey = in_journey?
+  end
+
+  def touch_out
+     @in_journey = !in_journey?
+  end
+
+  def in_journey?
+    true
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,10 @@ describe Oystercard do
     subject.top_up(50)
     subject
   end
+  let(:oyster_touched_in) do
+    oyster_with_balance.touch_in
+    oyster_with_balance
+  end
 
   context '#initialize' do
     it 'has a default balance of 0' do
@@ -27,6 +31,25 @@ describe Oystercard do
   context '#deduct_fare' do
     it 'deducts a fare from the balance' do
       expect{oyster_with_balance.deduct_fare(amount)}.to change { oyster_with_balance.balance }.by (-amount)
+    end
+  end
+
+  context '#in_journey?' do
+    it 'returns true if in journey' do
+      expect(subject).to be_in_journey
+    end
+  end
+
+  context '#touch_in' do
+    it 'should make in_journey? true' do
+      expect(oyster_touched_in).to be_in_journey
+    end
+  end
+
+  context '#touch_out' do
+    it 'should make in_journey? false' do
+      oyster_touched_in.touch_out
+      expect(oyster_touched_in.in_journey).to eq false
     end
   end
 end
